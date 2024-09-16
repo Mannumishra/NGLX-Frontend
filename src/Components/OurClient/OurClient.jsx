7f7b020344196e5f4d830a6ccbf50d49923c9085
mannumishra <mannumishra2207@outlook.com>
2024-09-16 14:25:01 +0530

    add chnages

--- src/Components/OurClient/OurClient.jsx
+++ src/Components/OurClient/OurClient.jsx
@@ -5,22 +5,22 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Autoplay } from 'swiper/modules';
 
-import s1 from './l1.webp'
-import s2 from './l2.png'
-import s3 from './l3.png'
-import s4 from './l4.svg'
-import s5 from './l5.png'
-import s6 from './l6.png'
-import s7 from './l7.png'
-import s8 from './l8.jpg'
-import s9 from './l9.jpg'
-import s10 from './l10.jpg'
-import s11 from './l11.jpg'
-import s12 from './l12.jpg'
+import s1 from './1.jpeg'
+import s2 from './2.jpeg'
+import s3 from './3.jpeg'
+import s4 from './4.jpeg'
+import s5 from './5.jpeg'
+import s6 from './5.jpeg'
+import s7 from './6.jpeg'
+import s8 from './7.jpeg'
+import s9 from './8.jpeg'
+// import s10 from './l10.jpg'
+// import s11 from './l11.jpg'
+// import s12 from './l12.jpg'
 
 function OurClient() {
     // Array of image sources for clients
-    const clientImages = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12];
+    const clientImages = [s1, s2, s3, s4, s5, s6, s7, s8, s9];
 
     return (
         <section className='client-section'>
